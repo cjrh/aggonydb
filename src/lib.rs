6c5bb90701f36a4f1e11e72cbd6d7b80d01680b6
Caleb Hattingh <caleb.hattingh@gmail.com>
2022-04-25 15:08:10 +1000

    Remove commented-out code

--- src/lib.rs
+++ src/lib.rs
@@ -10,28 +10,6 @@ pub async fn server() -> Result<()> {
     let mypool = db::MyPool::new().await?;
     schema::migrate().await?;
 
-    let row: (i64,) = sqlx::query_as("SELECT $1")
-        .bind(150_i64)
-        .fetch_one(&mypool.pool)
-        .await?;
-    assert_eq!(row.0, 150);
-
-    // let insert_sql = r#"
-    // INSERT INTO helloworld(id, set) VALUES (1, hll_empty())
-    // "#;
-    //
-    // let add_sql = r#"
-    // UPDATE helloworld SET set = hll_add(set, hll_hash_integer(12345)) WHERE id = 1;
-    // "#;
-    //
-    // mypool.q(insert_sql).await?;
-    // mypool.q(add_sql).await?;
-    //
-    // let row: (f64,) = sqlx::query_as("SELECT hll_cardinality(set) FROM helloworld WHERE id = 1")
-    //     .fetch_one(&mypool.pool)
-    //     .await?;
-    // println!("{}", row.0);
-
     let shared_pool = Arc::new(Mutex::new(mypool));
 
     // Start the web server
